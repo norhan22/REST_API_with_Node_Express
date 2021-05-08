14ed93ad5bab4c9d6ced6a1245c115015bbd35ed
norhan <nora.moh.2011@gmail.com>
2021-05-08 23:45:55 +0200

    using express module instead of http module

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 
 const
-  http = require('http'),
-  server = http.createServer((req, res) => {
-    if (req.url === '/') {
-      res.write('Node HTTP Module Hello World ')
-      res.end()
-    } else if (req.url === '/api/classes') {
-      res.write(JSON.stringify(
-        [
-          { id: 1, name: 'arabic' },
-          { id: 2, name: 'english' }
-        ]))
-      res.end()
-    }
-  })
+  express = require('express'),
+  server = express()
+  
+server.get('/', (req, res) => {
+  res.send('Node HTTP Module Hello World ')
+})
+server.get('/api/classes', (req, res) => {
+  res.send(
+    [
+      { id: 1, name: 'arabic' },
+      { id: 2, name: 'english' }
+    ]
+  )
+})
+
+
+// http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.write('Node HTTP Module Hello World ')
+//     res.end()
+//   } else if (req.url === '/api/classes') {
+//     res.write(JSON.stringify(
+//       [
+//         { id: 1, name: 'arabic' },
+//         { id: 2, name: 'english' }
+//       ]))
+//     res.end()
+//   }
+// })
   
 // Using On Listener &&  Socket Class ( Not recommended ) Dealing with REquest
 // && Response in createServer Method  more recommended  
 // server.on('connection', (socket) => { console.log('New Connection ....') })
 
-server.listen('3000')
+server.listen('3000', () => { console.log('listening on port 3000 ...') })
 console.log('Listen to server')
 
