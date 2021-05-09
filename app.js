48022e2d579f51f21237c6f1745a9bc33448de40
norhan <nora.moh.2011@gmail.com>
2021-05-09 05:22:32 +0200

    Handling GET && POST Requests Sections

--- app.js
+++ app.js
@@ -2,30 +2,49 @@
 const
   express = require('express'),
   server = express(),
-  port = process.env.PORT
+  port = process.env.PORT,
+  classes =  [
+    { id: 1, name: 'arabic' },
+    { id: 2, name: 'english' }
+  ],
+  resource = '/api/classes'
   
-  
+server.use(express.json) 
+
+// Show PORT ENV 
+server.listen(port, () => { console.log(`listening on port ${port} ....`) })
+
+////////////////////////////
+// Get Method Requests
+////////////////////////////
+// Parent dir
 server.get('/', (req, res) => {
   res.send('Node HTTP Module Hello World ')
 })
-server.get('/api/classes', (req, res) => {
-  res.send(
-    [
-      { id: 1, name: 'arabic' },
-      { id: 2, name: 'english' }
-    ]
-  )
-})
-// Route Parameters
-// server.get('/api/classes/:id/:name', (req, res) => {
-//   res.send(req.params)
-// })
 
-// Query string parameters
-server.get('/api/classes/:id/:name', (req, res) => {
-  res.send(req.query)
+
+// Get all classes
+server.get(resource, (req, res) => {
+  res.send(classes)
 })
 
-server.listen(port, () => { console.log(`listening on port ${port} ....`) })
-console.log('Listen to server')
+// Get a class by Id
+server.get(`${resource}/:id`, (req, res) => {
+  const id = parseInt(req.params.id),
+    matchedClass = classes.find(c => c.id === id)
+  if (matchedClass) res.send(matchedClass)
+  else res.status(404).send('Not found')
+})
+
+////////////////////////////
+// Post Method Requests
+////////////////////////////
+server.post(`${resource}`, (req, res) => {
+  const newClass = {
+    id: classes.length + 1,
+    name : req.body.name
+  }
+  classes.push(newClass)
+  res.send(newClass)
+})
 
